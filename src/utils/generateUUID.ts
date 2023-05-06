249a3572e9e02898dad867b2c4dec67f64253f5c
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-06 16:17:03 +0800

    refactor: updated UUID to use randomUUID

--- src/utils/generateUUID.ts
+++ src/utils/generateUUID.ts
@@ -1,7 +1,6 @@
-import crypto from 'crypto';
-import {IMetadata} from "../interfaces";
+import {randomUUID} from 'crypto';
 
 
 export const generateUUID = async (buffer: Buffer | undefined, metadata: Record<string, string>|undefined): Promise<string> => {
-    return crypto.randomBytes(16).toString('hex');
+    return randomUUID();
 }

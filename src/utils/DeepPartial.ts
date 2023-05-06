36eda089171d9a8ec359620aacf750eee693a10f
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2023-05-06 13:35:58 +0800

    feat: updated deepPartial method to include functions

--- src/utils/DeepPartial.ts
+++ src/utils/DeepPartial.ts
@@ -2,7 +2,20 @@ export type DeepPartial<T> = T extends object ? {
     [P in keyof T]?: DeepPartial<T[P]>;
 } : T;
 
+/**
+ *
+ * remove all properties with undefined or null values from nested objects
+ * @param obj
+ */
 export function prune(obj: any): any {
-    // remove all properties with undefined values from nested objects
-    return JSON.parse(JSON.stringify(obj));
+    if (obj === undefined) return undefined;
+    if (obj === null) return undefined;
+    if (typeof obj !== "object") return obj;
+    if (Array.isArray(obj)) return obj.map(prune);
+    const result: Record<string, any> = {};
+    for (const key of Object.keys(obj)) {
+        const value = prune(obj[key]);
+        if (value !== undefined) result[key] = value;
+    }
+    return result;
 }
